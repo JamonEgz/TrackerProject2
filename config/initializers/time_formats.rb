b62945fb101f832c43a3bcf6804fc0ad1dc9639d
JamesTGledhill <jamestgledhill@gmail.com>
2017-04-17 09:16:05 -0400

    changes to check ins page

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,9 @@
+
+# date_formats = {
+#   concise: '%d-%b-%Y' # 13-Jan-2014
+# }
+
+# Time::DATE_FORMATS.merge! date_formats
+# Date::DATE_FORMATS.merge! date_formats
+
+# Date::DATE_FORMATS[:month_day_comma_year] = "%B %e, %Y" 
