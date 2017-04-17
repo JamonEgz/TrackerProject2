46a3b1b1e1cbcd39227e2967119532c042e0f870
JamesTGledhill <jamestgledhill@gmail.com>
2017-04-17 10:47:25 -0400

    working nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   resources :check_ins
+  
+  	# get '/new_check_in' => 'check_ins#new', as: 'new_check_in'
+
+ 	get '/progress' => 'check_ins#index', as: 'progress'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
